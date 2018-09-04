e5110ac70993e794dbb3eac4ff9bee3fee6f367a
pap7tomek <pap7tomek@gmail.com>
2018-09-04 12:38:01 +0200

    poprawki

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'crud'
+    host     : 'sql7.freemysqlhosting.net',
+    user     : 'sql7255123',
+    password : '3PfmCqDlW6',
+    database : 'sql7255123'
 });
 //!qwe123Pa
 connection.connect(function(err) {
